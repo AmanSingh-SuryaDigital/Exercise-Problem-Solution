2a87c2f24e9f17d4c053dc39a0c88d9383c87789
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-11 13:11:35 +0530

    probelm4 commit1

--- src/test/java/org/example/Problem4Test.java
+++ src/test/java/org/example/Problem4Test.java
@@ -9,7 +9,6 @@ public class Problem4Test {
     @Test
     public void mixUpTest() {
         assertEquals( "pox mid",Problem4.mixUp("mix","pod"));
-//        System.out.println(mixUp("Aman","Singh"))
         assertEquals( "Sian Amngh",Problem4.mixUp("Aman","Singh"));
     }
 }
