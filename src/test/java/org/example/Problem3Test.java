1b5288d70c30756a22305ba5102541a51a662e59
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-12 18:16:02 +0530

    Update in the code by adding testcase

--- src/test/java/org/example/Problem3Test.java
+++ src/test/java/org/example/Problem3Test.java
@@ -9,8 +9,8 @@ public class Problem3Test {
 
     @Test
     public void fixStart() {
-        assertEquals( "ba**le" ,Problem3.fixStart("babble"));
-        assertEquals( "Ama**n" ,Problem3.fixStart("AmaAAn"));
-        assertNotEquals( "Amaaan" ,Problem3.fixStart("Amaaan"));
+        assertEquals("ba**le", Problem3.fixStart("babble"));
+        assertEquals("Ama**n", Problem3.fixStart("AmaAAn"));
+        assertNotEquals("Amaaan", Problem3.fixStart("Amaaan"));
     }
 }
