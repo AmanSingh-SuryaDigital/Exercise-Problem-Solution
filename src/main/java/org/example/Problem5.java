6bbcbf1fd55a2c34c291ff46d6daff26c616aa8b
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-13 15:54:28 +0530

    Update in the variable name

--- src/main/java/org/example/Problem5.java
+++ src/main/java/org/example/Problem5.java
@@ -2,16 +2,17 @@ package org.example;
 
 public class Problem5 {
 
-    public static String verbing(String s){
-        if (s.length() < 3) {
-            return s;
-        } else if (s.endsWith("ing")) {
-            return s + "ly";
+    public static String verbing(String stringValue1){
+        if (stringValue1.length() < 3) {
+            return stringValue1;
+        } else if (stringValue1.endsWith("ing")) {
+            return stringValue1 + "ly";
         } else {
-            return s + "ing";
+            return stringValue1 + "ing";
         }
 
     }
+
     public static void main(String[] args) {
         System.out.println(verbing("Helloworld"));
         System.out.println(verbing("Sing"));
