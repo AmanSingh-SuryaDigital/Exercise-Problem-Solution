3bd48b5bc3482ee838639d69e6d2458b41987734
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-13 14:43:34 +0530

    Again update in the file

--- src/test/java/org/example/Problem1Test.java
+++ src/test/java/org/example/Problem1Test.java
@@ -10,18 +10,13 @@ public class Problem1Test {
 
     @Test
     public void donutsTest() {
-<<<<<<< HEAD
-        Problem1 problem1 = new Problem1();
-        assertEquals("Number of donuts: 5", problem1.donuts(5));
-        assertEquals("Number of donuts: many", problem1.donuts(23));
 
-=======
         Problem1 pr = new Problem1();
 
         assertEquals("Number of donuts: 5",pr.donuts(5));
         assertEquals("Number of donuts: many",pr.donuts(23));
         assertEquals("Number of donuts: 0",pr.donuts(-1));
->>>>>>> ae48567ee2abf36a74fb637fd7876d4f542db0aa
+
     }
 
 }
