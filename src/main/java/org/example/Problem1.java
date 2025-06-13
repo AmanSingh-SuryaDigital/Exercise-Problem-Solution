8ce5b031b660a43a1a21f3c1e9f9902d8feb244b
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-13 13:55:43 +0530

    Revert "Problem 1 New Commit"
    This reverts commit 3b985a3e

---
+++ src/main/java/org/example/Problem1.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Problem1 {
+
+    public  String donuts(int count) {
+        if (count >= 10) {
+            return "Number of donuts: many";
+        } else {
+            return "Number of donuts: " + count;
+        }
+    }
+
+    public static void main(String[] args) {
+        Problem1 p = new Problem1();
+
+        System.out.println(p.donuts(5));
+        System.out.println(p.donuts(23));
+    }
+}
---
+++ src/test/java/org/example/Problem1Test.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class Problem1Test {
+
+    @Test
+    public void donutsTest() {
+        Problem1 pr = new Problem1();
+        assertEquals("Number of donuts: 5",pr.donuts(5));
+        assertEquals("Number of donuts: many",pr.donuts(23));
+        assertEquals("Number of donuts: -1",pr.donuts(-1));
+    }
+}
+
