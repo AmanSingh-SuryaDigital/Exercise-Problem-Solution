65ca8cf60ceae4e9ba016d94111a43d34117261d
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-13 14:40:09 +0530

    Update in the file

--- src/main/java/org/example/Problem1.java
+++ src/main/java/org/example/Problem1.java
@@ -1,14 +1,10 @@
 package org.example;
 
 public class Problem1 {
-<<<<<<< HEAD
     public  String donuts(int count) {
-        if (count < 0) {
-            throw new IllegalArgumentException("Count cannot be negative");
-        }
-=======
+
+
     public String donuts(int count) {
->>>>>>> ae48567ee2abf36a74fb637fd7876d4f542db0aa
         if (count >= 10) {
             return "Number of donuts: many";
         } else {
@@ -19,15 +15,11 @@ public class Problem1 {
     public static void main(String[] args) {
         Problem1 problem1 = new Problem1();
 
-<<<<<<< HEAD
         System.out.println(problem1.donuts(5));
         System.out.println(problem1.donuts(23));
         System.out.println(problem1.donuts(-2));
-=======
-        System.out.println(p.donuts(5));
-        System.out.println(p.donuts(23));
 
->>>>>>> ae48567ee2abf36a74fb637fd7876d4f542db0aa
+
     }
 }
 
