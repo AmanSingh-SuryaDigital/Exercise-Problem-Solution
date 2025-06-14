7a111af884ba230020476041e81574059c8be9e0
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-14 13:01:45 +0530

    Updating by changing the name of the variable

--- src/main/java/org/example/Problem4.java
+++ src/main/java/org/example/Problem4.java
@@ -2,20 +2,18 @@ package org.example;
 
 public class Problem4 {
 
-    public static String mixUp(String a, String b){
+    public static String mixUp(String firstWord, String secondWord) {
 
-        String c= b.substring(0,2)+a.substring(2);
-        String d= a.substring(0,2)+b.substring(2);
+        String swappedFirst = secondWord.substring(0, 2) + firstWord.substring(2);
+        String swappedSecond = firstWord.substring(0, 2) + secondWord.substring(2);
 
-        return c+ " "+d;
+        return swappedFirst + " " + swappedSecond;
     }
 
     public static void main(String[] args) {
 
-        System.out.println(mixUp("mix","pod"));
-        System.out.println(mixUp("Aman","Singh"));
-
-
+        System.out.println(mixUp("mix", "pod"));
+        System.out.println(mixUp("Aman", "Singh"));
     }
 
 }
