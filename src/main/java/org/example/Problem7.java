d091893a91d1a77fc9e0da86429ff55739929386
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-14 13:11:43 +0530

    Updating by changing the name of the variable

--- src/main/java/org/example/Problem7.java
+++ src/main/java/org/example/Problem7.java
@@ -2,24 +2,27 @@ package org.example;
 
 public class Problem7 {
 
-    public static String frontBack(String stringValue1, String stringValue2){
+    public static String frontBack(String first, String second) {
 
-        int stringValue3 = stringValue1.length()/2;
-        int stringValue4= stringValue2.length()/2;
+        int firstMid = first.length() / 2;
+        int secondMid = second.length() / 2;
 
-        if(stringValue1.length()%2==1){
-            stringValue3=stringValue3+1;
+        if (first.length() % 2 == 1) {
+            firstMid += 1;
         }
-        if(stringValue2.length()%2==1){
-            stringValue4=stringValue4+1;
-        }
-        String output1= stringValue1.substring(0,stringValue3);
-        String output2= stringValue2.substring(0,stringValue4);
-        String output3=stringValue1.substring(stringValue3);
-        String output4=stringValue2.substring(stringValue4);
 
-        return output1+output2+output3+output4;
+        if (second.length() % 2 == 1) {
+            secondMid += 1;
+        }
+
+        String firstFront = first.substring(0, firstMid);
+        String secondFront = second.substring(0, secondMid);
+        String firstBack = first.substring(firstMid);
+        String secondBack = second.substring(secondMid);
+
+        return firstFront + secondFront + firstBack + secondBack;
     }
+
     public static void main(String[] args) {
 
         System.out.println(frontBack("abcd","xy"));
