952813e6d1ae57dc064b593fcbf0a47406aee18a
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-16 17:11:28 +0530

    Updating by fixing the name and EOF issue in the file

--- src/main/java/org/example/Problem3.java
+++ src/main/java/org/example/Problem3.java
@@ -3,19 +3,20 @@ package org.example;
 public class Problem3 {
     public static String replaceRepeatedFirstChar(String inputString) {
 
-        StringBuilder modifiedString = new StringBuilder(inputString.substring(0, 1));
+        StringBuilder modifiedStringBuilder = new StringBuilder(inputString.substring(0, 1));
+
         char initialCharacter = inputString.charAt(0);
 
         for (int i = 1; i < inputString.length(); i++) {
             char currentChar = inputString.charAt(i);
             if (currentChar == initialCharacter) {
-                modifiedString.append("*");
+                modifiedStringBuilder.append("*");
             } else {
-                modifiedString.append(currentChar);
+                modifiedStringBuilder.append(currentChar);
             }
         }
 
-        return modifiedString.toString();
+        return modifiedStringBuilder.toString();
     }
 
     public static void main(String[] args) {
