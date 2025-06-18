7ff7d16fc6ff942f2303d2d99e7bb3804a846e17
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-18 18:08:33 +0530

    Update regarding length of string

--- src/main/java/org/example/Problem4.java
+++ src/main/java/org/example/Problem4.java
@@ -4,6 +4,10 @@ public class Problem4 {
 
     public static String swapFirstTwoCharacters(String firstWord, String secondWord) {
 
+        if(firstWord.length() < 2 && secondWord.length() < 2){
+            return "Input length should be more than 2";
+        }
+
         String modifiedInputOne = secondWord.substring(0, 2) + firstWord.substring(2);
         String modifiedInputSecond = firstWord.substring(0, 2) + secondWord.substring(2);
 
@@ -14,6 +18,7 @@ public class Problem4 {
 
         System.out.println(swapFirstTwoCharacters("mix", "pod"));
         System.out.println(swapFirstTwoCharacters("Aman", "Singh"));
+        System.out.println(swapFirstTwoCharacters("ab","cd"));
     }
 
 }
