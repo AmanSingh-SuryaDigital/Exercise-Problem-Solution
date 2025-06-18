f6da87fe6827648bbf23724c26df0a934b9eed27
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-18 17:55:57 +0530

    Update by adding feature related to casing

--- src/main/java/org/example/Problem5.java
+++ src/main/java/org/example/Problem5.java
@@ -6,17 +6,17 @@ public class Problem5 {
 
         if (inputWord.length() < 3) {
             return inputWord;
-        } else if (inputWord.replaceAll("\\s","").endsWith("ing")) {
-            return inputWord.replaceAll("\\s","") + "ly";
+        } else if (inputWord.replaceAll("\\s","").toLowerCase().endsWith("ing")) {
+            return inputWord.replaceAll("\\s","").toLowerCase() + "ly";
         } else {
-            return inputWord.replaceAll("\\s","") + "ing";
+            return inputWord.replaceAll("\\s","").toLowerCase() + "ing";
         }
     }
 
     public static void main(String[] args) {
         System.out.println(addVerbSuffix("Helloworlding "));
         System.out.println(addVerbSuffix("Sing"));
-        System.out.println(addVerbSuffix("Helloworld ing "));
+        System.out.println(addVerbSuffix("Helloworld ING "));
 
     }
 
