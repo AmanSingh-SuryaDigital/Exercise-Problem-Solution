5ceba0824e1367d6cc53c882918644351f0a530f
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-18 17:18:56 +0530

    Update by adding extra feature

--- src/main/java/org/example/Problem5.java
+++ src/main/java/org/example/Problem5.java
@@ -5,16 +5,18 @@ public class Problem5 {
     public static String addVerbSuffix(String inputWord) {
         if (inputWord.length() < 3) {
             return inputWord;
-        } else if (inputWord.endsWith("ing")) {
-            return inputWord + "ly";
+        } else if (inputWord.replaceAll("\\s","").endsWith("ing")) {
+            return inputWord.replaceAll("\\s","") + "ly";
         } else {
-            return inputWord + "ing";
+            return inputWord.replaceAll("\\s","") + "ing";
         }
     }
 
     public static void main(String[] args) {
-        System.out.println(addVerbSuffix("Helloworld"));
+        System.out.println(addVerbSuffix("Helloworlding "));
         System.out.println(addVerbSuffix("Sing"));
+        System.out.println(addVerbSuffix("Helloworld ing "));
+
     }
 
 }
