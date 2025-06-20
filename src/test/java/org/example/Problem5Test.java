07a5fd9d4f89ad910d5355face01f01675640f96
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-20 12:35:04 +0530

    Update by adding trim feature in the program

--- src/test/java/org/example/Problem5Test.java
+++ src/test/java/org/example/Problem5Test.java
@@ -9,9 +9,11 @@ public class Problem5Test {
     @Test
     public void addVerbSuffixTest() {
         assertEquals("Helloworlding",Problem5.addVerbSuffix("Helloworld"));
-        assertEquals("Singly",Problem5.addVerbSuffix("Sing"));
+        assertEquals("SIngly",Problem5.addVerbSuffix("SIng"));
         assertEquals("Helloworld INGly",Problem5.addVerbSuffix("Helloworld ING "));
+        assertEquals("Hello worldingly",Problem5.addVerbSuffix("Hello worlding "));
         assertEquals("SiNgly",Problem5.addVerbSuffix("SiNg "));
-
+        assertEquals("Hello world in ging",Problem5.addVerbSuffix("Hello world in g"));
+        assertEquals("Hello worlding",Problem5.addVerbSuffix("Hello world "));
     }
 }
