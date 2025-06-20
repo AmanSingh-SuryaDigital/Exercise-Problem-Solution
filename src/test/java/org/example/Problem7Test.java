4df063f48637cb8ba73628fc633ddef2c7b5949b
AmanSingh-SuryaDigital <aman.s@Surya-digital.com>
2025-06-20 17:24:33 +0530

    Adding different test case

--- src/test/java/org/example/Problem7Test.java
+++ src/test/java/org/example/Problem7Test.java
@@ -10,5 +10,10 @@ public class Problem7Test {
     public void frontBackTest() {
        assertEquals ("abxcdy", Problem7.mergeFrontAndBackHalves("abcd","xy"));
        assertEquals ("abcdxyefgz", Problem7.mergeFrontAndBackHalves("abcdefg","xyz"));
+       assertEquals ("Input length is not valid ,Length should be more than equals to 2", Problem7.mergeFrontAndBackHalves("a","z"));
+       assertEquals ("axyz", Problem7.mergeFrontAndBackHalves("a","xyz"));
+       assertEquals ("a x byz", Problem7.mergeFrontAndBackHalves("a b","x yz"));
+       assertEquals ("a wx b yz", Problem7.mergeFrontAndBackHalves("a b ","wx yz"));
+
     }
 }
